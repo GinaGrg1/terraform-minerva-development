0a44d6d2c5e3468d3ad6e46a49e2ff1169ae0040
GurungRegina <gina524286@gmail.com>
2025-12-22 16:30:43 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,3 @@ output "storage_account_id" {
     value = azurerm_storage_account.securergstorage.id
     description = "The storage account ID"
 }
-
-  git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
