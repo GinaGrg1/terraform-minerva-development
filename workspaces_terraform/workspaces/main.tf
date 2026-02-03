3ad94768474feadead7135bac4fa6734b48e01bf
Regina Gurung <gina524286@gmail.com>
2026-02-03 12:17:22 +0000

    using resource_group_data

--- workspaces_terraform/workspaces/main.tf
+++ workspaces_terraform/workspaces/main.tf
@@ -45,6 +45,7 @@ resource "azurerm_resource_group" "main" {
 module "resource_group" {
     source = "./Modules/resource_group"
 
-    resource_group_name = "rg-dev"
-    resource_group_location = "westus"
+    resource_group_name = var.resource_group_data["development_01"].name
+    resource_group_location = var.resource_group_data["development_01"].location
+    resource_group_data = var.resource_group_data
 }
